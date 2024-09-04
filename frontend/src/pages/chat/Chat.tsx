16cb3d936cc10a1b49a6b416dd5333a7857fc08c
Hendrik Carius <62423832+henca312@users.noreply.github.com>
2024-09-04 11:07:32 +0200

    Update Chat.tsx
    Umstellen radial-gradient Verlauf auf Dunkelblau
    
    //'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)'
                              '#0F6CBD'

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -995,7 +995,8 @@ const Chat = () => {
                       root: {
                         color: '#FFFFFF',
                         background:
-                          'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)'
+                          //'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)'
+                          '#0F6CBD'
                       },
                       rootDisabled: {
                         background: '#F0F0F0'
@@ -1020,8 +1021,8 @@ const Chat = () => {
                     root: {
                       color: '#FFFFFF',
                       background:
-                        'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)'
-                    },
+                          //'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)'
+                          '#0F6CBD'                    },
                     rootDisabled: {
                       background: '#F0F0F0'
                     }
